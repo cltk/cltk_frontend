e03368238c7d44fe8d939ef3ede216037332de95
Luke Hollis <lukehollis@gmail.com>
2016-02-14 15:20:00 -0500

    make reading content panel react to presence of definitions and commentary panel

--- client/views/reading/reading.jsx
+++ client/views/reading/reading.jsx
@@ -126,12 +126,14 @@ Template.headerReading.events = {
       $target.removeClass("checked");
       if ($target.data().type === "definitions") {
         $(".definitions-panel").removeClass("slide-visible");
+        $("#reading").removeClass("with-definitions-shown");
 
       } else if ($target.data().type === "commentary") {
         $(".commentary-panel").removeClass("commentary-visible");
 
         if(!$(".commentary-panel").hasClass("translations-visible")){
           $(".commentary-panel").removeClass("slide-visible");
+          $("#reading").removeClass("with-commentary-shown");
 
         }
 
@@ -140,6 +142,7 @@ Template.headerReading.events = {
 
         if(!$(".commentary-panel").hasClass("commentary-visible")){
           $(".commentary-panel").removeClass("slide-visible");
+          $("#reading").removeClass("with-commentary-shown");
 
         }
 
@@ -149,14 +152,17 @@ Template.headerReading.events = {
       $target.addClass("checked");
       if ($target.data().type === "definitions") {
         $(".definitions-panel").addClass("slide-visible");
+        $("#reading").addClass("with-definitions-shown");
 
       } else if ($target.data().type === "commentary") {
         $(".commentary-panel").addClass("slide-visible");
         $(".commentary-panel").addClass("commentary-visible");
+        $("#reading").addClass("with-commentary-shown");
 
       } else if ($target.data().type === "translations") {
         $(".commentary-panel").addClass("slide-visible");
         $(".commentary-panel").addClass("translations-visible");
+        $("#reading").addClass("with-commentary-shown");
 
       }
 
