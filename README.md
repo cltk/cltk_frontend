fa3660423d495bb03022f21ef594ead4150a95d8
Luke Hollis <lukehollis@gmail.com>
2016-03-02 01:37:01 -0500

    add styles for related passages and entities; update readme briefly to provide more information about the project

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 
 The Classics Archive application is currently under active development and is not ready for production.
 
-# About the Classics Archive application
+# About  
 
-Very briefly, to get started developing, first install Meteor.  Then clone this repo and in the application root directory run "meteor" to start the application.
+The goal of this application is to provide a modern reading environment for documents included in the CLTK corpora. Dev builds will be put up at http://api.cltk.org for the early stages of development.  Currently all demo data in the application is static, but in the future, text data will be consumed by the API under development at this repository: https://github.com/cltk/cltk_api
+
+
+# Development
+
+The CLTK frontend reading environment is built with Meteor and React.  
+
+Very briefly, to get started developing, first [install Meteor](https://www.meteor.com/install).  Then clone this repo and in the application root directory run "meteor" to start the application.
 
 For managing lists of outstanding development items, we're using ZenHub: https://www.zenhub.io/.  If you're interested in contributing, please consider installing the ZenHub plugin for your browser.
--- client/views/reading/ReadingText.jsx
+++ client/views/reading/ReadingText.jsx
@@ -70,27 +70,65 @@ ReadingText = React.createClass({
           <div className="text-meta text-related-passages">
             <div className="related-passage">
               <span className="related-passage-edition">1920, A. S. F. Gow</span>
-              <a href="#" className="related-passage-ref">
+              <a href="#" className="related-passage-ref paper-link">
                 <h4 >Theocritus, Idylls 17.2-3</h4>
               </a>
               <div className="related-passage-lemma">
                 <p>ἄλλοκα δ’ αὖ ποτὶ τὸν ῥιπτεῖ νόον· οἳ δ’ ὑπ’ ἔρωτος</p>
                 <p>δηθὰ κυλοιδιόωντες ἐτώσια μοχθίζοντι.</p>
-                
+
               </div>
 
             </div>
 
           </div>
           <div className="text-meta text-entities">
-            <div className="entity">
-              <div className="entity-thumnail-image image-wrap">
+            <div className="entity-teaser">
+              <a href="#" className="entity-thumbnail-image image-wrap paper-shadow">
                 <img src="/images/entities/herodotus_thumbnail.jpg"/>
-              </div>
+              </a>
               <div className="entity-description">
-                <h4 className="related-passage-ref">Herodotus</h4>
-                <p>Herodotus was a Greek historian who was born in Halicarnassus, Caria ...</p>
-                <a href="#" className="entity-read-more">Read more</a>
+                <a href="#" className="entity-name ">
+                  <h4 >Herodotus</h4>
+                </a>
+                <span className="entity-bio">Herodotus was a Greek historian who was born in Halicarnassus, Caria and lived in the 5th century BC, a contemporary of Socrates ... </span>
+
+                <div className="entity-meta">
+                  <a href="#" className="entity-meta-link entity-additional-refernces entity-action md-button md-ink-ripple">
+                    <span>76 other references</span>
+                    <div className="md-ripple-container"></div>
+                  </a>
+                  <a href="#" className="entity-meta-link entity-media entit-action md-button md-ink-ripple">
+                    <span>8 associated media</span>
+                    <div className="md-ripple-container"></div>
+                  </a>
+
+                </div>
+              </div>
+
+            </div>
+            <div className="entity-teaser">
+              <a href="#" className="entity-thumbnail-image image-wrap paper-shadow">
+                <img src="/images/entities/halicarnassus_thumbnail.jpg"/>
+              </a>
+              <div className="entity-description">
+                <a href="#" className="entity-name ">
+                  <h4 >Halicarnassus</h4>
+                </a>
+                <span className="entity-bio">Halicarnassus was an ancient Greek city at the site of modern Bodrum in Turkey. It is located in southwest Caria on a picturesque ... </span>
+
+                <div className="entity-meta">
+                  <a href="#" className="entity-meta-link entity-additional-refernces entity-action md-button md-ink-ripple">
+                    <span>24 other references</span>
+                    <div className="md-ripple-container"></div>
+                  </a>
+                  <a href="#" className="entity-meta-link entity-media entit-action md-button md-ink-ripple">
+                    <span>3 associated media</span>
+                    <div className="md-ripple-container"></div>
+                  </a>
+
+                </div>
+
               </div>
 
             </div>
