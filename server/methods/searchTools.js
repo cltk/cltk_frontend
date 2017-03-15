93083d9e58c96f94d331f0b4b8beae0c9fa2f8ad
pletcher <charles.pletcher@gmail.com>
2017-03-15 14:46:54 -0400

    Add a server-side cache
    Fixes #99
    
    Adds a server-side Cache class and corresponding 'cache' collection,
    and updates searchTools() to utilize the cache for prepopulating
    the search page.

--- server/methods/searchTools.js
+++ server/methods/searchTools.js
@@ -1,39 +1,62 @@
 import Authors from '/imports/collections/authors';
 import Works from '/imports/collections/works';
 
+import cache from '/server/cache';
+
+const CACHE_KEY = 'searchTools_cache_key'
+
 Meteor.methods({
 	searchTools() {
-		let languages = [];
-		let corpora = [];
-		let author = {};
-		const authors = [];
+		const cachedResults = cache.get(CACHE_KEY);
 
-		languages = _.uniq(Works.find({ workLanguage: { $exists: true } }, {
-			sort: { workLanguage: 1 }, fields: { workLanguage: true },
-		}).fetch().map((x) => x.workLanguage), true);
-		corpora = _.uniq(Works.find({ corpus: { $exists: true } }, {
-			sort: { corpus: 1 }, fields: { corpus: true },
-		}).fetch().map((x) => x.corpus), true);
-		worksAuthorsRaw = Works.find({ authors: { $exists: true } }, {
-			sort: { 'authors.english_name': 1 }, fields: { authors: true },
-		}).fetch().map((x) => x.authors);
+		if (cachedResults) {
+			return {
+				...cachedResults,
+				authors: Authors.find({ _id: { $in: cachedResults.authors } }).fetch()
+			};
+		}
 
-		worksAuthorsRaw.forEach((authorsRaw) => {
-			authorsRaw.forEach((authorRaw) => {
-				author = Authors.findOne({ _id: authorRaw });
-				if (author && !(authors.some((existingAuthor) =>
-					existingAuthor._id._str === author._id._str
-				))) {
-					authors.push(author);
-				}
-			});
+		// NOTE: Works._collection.rawCollection() will only work on the server.
+		// This is fine for now, because this file only runs on the server, but
+		// there would need to be a workaround (or a `Meteor.isServer` check) if
+		// the browser wanted to run this file.
+		const languagesPromise = Works._collection.rawCollection().distinct('workLanguage', {
+			workLanguage: { $nin: ['', null] }
 		});
+		const corporaPromise = Works._collection.rawCollection().distinct('corpus', {
+			corpus: { $nin: ['', null] }
+		});
+		const worksAuthorsRaw = Works.find({ authors: { $exists: true } }, {
+			sort: { 'authors.english_name': 1 },
+			fields: { authors: true },
+		}).map(doc => doc.authors[0]);
 
-		return {
-			languages,
-			corpora,
-			authors,
-		};
+		return Promise.all([
+			languagesPromise,
+			corporaPromise
+		]).then(([ languages, corpora ]) => {
+			cache.set(CACHE_KEY, {
+				...results,
+				// `languages` and `corpora` are reasonably small, and
+				// unlikely to grow dramatically, so it seems okay to
+				// store them as is in the cache. Storing `authors`
+				// wholesale, on the other hand, would dramatically
+				// increase the size of the cached document, so just
+				// store the `_id`s.
+				authors: worksAuthorsRaw
+			});
+
+			const authorsRaw = Authors.find({ _id: { $in: worksAuthorsRaw } }).fetch();
+			const authors = Array.from(new Set(authorsRaw));
+
+			return {
+				languages,
+				corpora,
+				authors,
+			};
+		}).catch(reason => {
+			console.log(reason);
+		});
 	},
 
 });
