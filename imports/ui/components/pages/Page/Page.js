592e72d6f794af2d3c87bb7f27eb40bfa0c53745
pletcher <charles.pletcher@gmail.com>
2017-07-06 14:30:31 -0400

    Update packages to try to squash some errors
    Remove .vscode

--- imports/ui/components/pages/Page/Page.js
+++ imports/ui/components/pages/Page/Page.js
@@ -17,79 +17,77 @@ import BackgroundImageHolder from '/imports/ui/components/pages/BackgroundImageH
 import LoadingPage from '/imports/ui/components/pages/LoadingPage';
 
 
-const Page = React.createClass({
-	propTypes: {
-		slug: React.PropTypes.string,
-		page: React.PropTypes.object,
-		ready: React.PropTypes.bool,
-		images: React.PropTypes.array,
-		thumbnails: React.PropTypes.array,
-		loading: React.PropTypes.bool,
-		settings: React.PropTypes.object,
-	},
+const Page = props => {
+	const { page, settings, slug, loading } = props;
+	const headerImageUrl = '/images/apotheosis_homer.jpg';
 
-	render() {
-		const { page, settings, slug, loading } = this.props;
-		const headerImageUrl = '/images/apotheosis_homer.jpg';
-
-		if (loading) {
-			return (
-				<LoadingPage />
-			);
-		} else if (!loading && !page) {
-			return (
-				<NotFound
-					isTest={slug === '__test__'}
-				/>
-			);
-		}
-
-		if (page && page.title) {
-			Utils.setTitle(`${page.title} | ${settings.title}`);
-		}
-		if (headerImageUrl) {
-			Utils.setMetaImage(headerImageUrl);
-		}
-
+	if (loading) {
 		return (
-			// todo: return 404 if !page.length
-			<div className={`page page-${slug} content primary`}>
+			<LoadingPage />
+		);
+	} else if (!loading && !page) {
+		return (
+			<NotFound
+				isTest={slug === '__test__'}
+			/>
+		);
+	}
 
-				<section className="block header header-page cover parallax">
-					<BackgroundImageHolder
-						imgSrc="/images/apotheosis_homer.jpg"
-					/>
+	if (page && page.title) {
+		Utils.setTitle(`${page.title} | ${settings.title}`);
+	}
+	if (headerImageUrl) {
+		Utils.setMetaImage(headerImageUrl);
+	}
 
-					<div className="container v-align-transform">
-						<div className="grid inner">
-							<div className="center-content">
-								<div className="page-title-wrap">
-									<h1 className="page-title">
-										{page.title}
-									</h1>
-									<h2>
-										{page.subtitle}
-									</h2>
-								</div>
+	return (
+		// todo: return 404 if !page.length
+		<div className={`page page-${slug} content primary`}>
+
+			<section className="block header header-page cover parallax">
+				<BackgroundImageHolder
+					imgSrc="/images/apotheosis_homer.jpg"
+				/>
+
+				<div className="container v-align-transform">
+					<div className="grid inner">
+						<div className="center-content">
+							<div className="page-title-wrap">
+								<h1 className="page-title">
+									{page.title}
+								</h1>
+								<h2>
+									{page.subtitle}
+								</h2>
 							</div>
 						</div>
 					</div>
-				</section>
+				</div>
+			</section>
 
-				<section className="page-content container">
-					{page.byline ?
-						<div className="page-byline">
-							<h3>
-								{page.byline}
-							</h3>
-						</div>
-						: ''}
-					<div dangerouslySetInnerHTML={{ __html: page.content }} />
-				</section>
-			</div>
-		);
-	},
-});
+			<section className="page-content container">
+				{page.byline ?
+					<div className="page-byline">
+						<h3>
+							{page.byline}
+						</h3>
+					</div>
+					: ''}
+				<div dangerouslySetInnerHTML={{ __html: page.content }} />
+			</section>
+		</div>
+	);
+};
+
+Page.propTypes = {
+	slug: React.PropTypes.string,
+	page: React.PropTypes.object,
+	ready: React.PropTypes.bool,
+	images: React.PropTypes.array,
+	thumbnails: React.PropTypes.array,
+	loading: React.PropTypes.bool,
+	settings: React.PropTypes.object,
+};
 
 const pageContainer = createContainer(({ slug }) => {
 	let page = {};
